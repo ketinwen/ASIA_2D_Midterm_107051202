a0d77df31b0607e0340d78f7f379ba386a9578c2
Amipower <71679909+Amipower@users.noreply.github.com>
2020-11-17 05:29:18 +0800

    套用攝影機

--- 2D_Midterm_107051202/Assets/Scripts/Player.cs
+++ 2D_Midterm_107051202/Assets/Scripts/Player.cs
@@ -1,12 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public GameObject final;
+    public Text textcount;
+    public int count;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print("碰到傳送門");
+        if (collision.name == "傳送門")
+        {
+            final.SetActive(true);
+        }
+
+        if (collision.tag == "星星")
+        {
+            Destroy(collision.gameObject);
+            count++;
+            textcount.text = "星星數量:" + count;
+        }
     }
 
 }
